f1c3c4ff137ff5abfa9967852df3f00b5f0dc7e5
Vinicius Alvim <vini0191@gmail.com>
2021-02-08 16:44:34 -0300

    feat: add Header and Nav components

--- components/Page.js
+++ components/Page.js
@@ -1,6 +1,9 @@
+import Header from './Header';
+
 export default function Page({ children, cool }) {
   return (
     <div>
+      <Header />
       <h2>I'm the page component</h2>
       {children}
       {cool}
